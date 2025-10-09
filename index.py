ed01709502e75f61b8bfe39d815e28b885fc6a54
suvadeep🚀 <suvadeepm0709@gmail.com>
2025-10-10 00:14:30 +0530

    Add Notes on  Python Variables

--- index.py
+++ index.py
@@ -1,2 +1,29 @@
-print(1+1)
-print("Hello World!")
+prize_money = 625
+
+prize_money+= 5
+print(prize_money)
+#prize_money = 630
+
+prize_money-=5
+print(prize_money) 
+#prize_money = 625
+
+prize_money*=2
+print(prize_money)
+# prize_money = 1250
+
+prize_money/=2
+print(prize_money)
+# prize_money = 625
+
+prize_money//= 2
+print(prize_money)
+#prize_money = 312
+
+prize_money**=2
+print(prize_money)
+#prize_money = 97344
+
+prize_money%=2
+print(prize_money)
+#prize_money = 0.0
